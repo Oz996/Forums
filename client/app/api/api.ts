29013bab251569e31d9f0f9e11f4969c9fc14083
Oz996 <ozgun996@hotmail.com>
2023-11-19 16:20:21 +0100

    testing util url function for hosting

--- client/app/api/api.ts
+++ client/app/api/api.ts
@@ -1,28 +1,31 @@
+import { getBaseUrl } from "@/lib/utils/URL";
 import axios from "axios";
 
 export const getPosts = async () => {
   try {
-    const data = await axios.get("https://next-forums.vercel.app/api/posts");
+    const data = await axios.get(getBaseUrl() + "/api/posts");
     return data;
   } catch (error) {
     console.error(error);
   }
 };
+// "https://next-forums.vercel.app/api/posts"
 
 export const getPost = async (id: string) => {
   try {
-    const data = await axios.get(`http://localhost:3000/api/post/${id}`);
+    const data = await axios.get(getBaseUrl() + `/api/post/${id}`);
     return data;
   } catch (error) {
     console.error(error);
   }
 };
-
+// `http://localhost:3000/api/post/${id}`
 export const getUser = async (id: string) => {
   try {
-    const data = await axios.get(`http://localhost:3000/api/user/${id}`);
+    const data = await axios.get(getBaseUrl() + `/api/user/${id}`);
     return data;
   } catch (error) {
     console.error(error);
   }
 };
+// `http://localhost:3000/api/user/${id}`
