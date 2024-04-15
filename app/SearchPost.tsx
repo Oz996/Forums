1a7a4f7e98b52fa763e3ac2db4314ce50657c286
Oz996 <ozgun996@hotmail.com>
2024-04-15 20:13:04 +0200

    removed unnecessary loader for search/filter div

--- app/SearchPost.tsx
+++ app/SearchPost.tsx
@@ -1,12 +1,11 @@
 import { Categories } from "@/types";
-import { Card, Input, Select, SelectItem, Skeleton } from "@nextui-org/react";
+import { Card, Input, Select, SelectItem } from "@nextui-org/react";
 import { Dispatch, SetStateAction } from "react";
 
 interface props {
-  isLoading: boolean;
   setSearch: Dispatch<SetStateAction<string>>;
 }
-export default function SearchPost({ setSearch, isLoading }: props) {
+export default function SearchPost({ setSearch }: props) {
   const categories: Categories[] = [
     { id: 0, value: "", name: "All" },
     { id: 1, value: "red", name: "Red" },
@@ -16,29 +15,27 @@ export default function SearchPost({ setSearch, isLoading }: props) {
   ];
 
   return (
-    <Skeleton isLoaded={!isLoading} className="rounded-xl">
-      <Card className="p-10">
-        <div className="flex flex-col md:flex-row gap-3">
-          <div className="md:w-[30%]">
-            <Select
-              label="Select Category"
-              onChange={(e) => setSearch(e.target.value)}
-            >
-              {categories.map((category) => (
-                <SelectItem key={category.value} value={category.value}>
-                  {category.name}
-                </SelectItem>
-              ))}
-            </Select>
-          </div>
-          <div className="md:w-[70%]">
-            <Input
-              onChange={(e) => setSearch(e.target.value)}
-              label="Search for post..."
-            />
-          </div>
+    <Card className="p-10">
+      <div className="flex flex-col md:flex-row gap-3">
+        <div className="md:w-[30%]">
+          <Select
+            label="Select Category"
+            onChange={(e) => setSearch(e.target.value)}
+          >
+            {categories.map((category) => (
+              <SelectItem key={category.value} value={category.value}>
+                {category.name}
+              </SelectItem>
+            ))}
+          </Select>
         </div>
-      </Card>
-    </Skeleton>
+        <div className="md:w-[70%]">
+          <Input
+            onChange={(e) => setSearch(e.target.value)}
+            label="Search for post..."
+          />
+        </div>
+      </div>
+    </Card>
   );
 }
